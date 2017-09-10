c9a6798aeec76445eae40b947a36241beefc585a
kharazzoro <kha.1991.14.2017@gmail.com>
2017-09-10 16:22:47 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -4,41 +4,37 @@ const dbClient = require('../helper/dbClient.js');
 
 /* GET home page. */
 
-router.get('/', function (req, res, next) {
-  const callBack = (error, products) => {
-    if (error) {
-      res.sendStatus(500)
+router.get('/', function(req, res, next) {
+    const callBack = (error, products) => {
+        if (error) {
+            res.sendStatus(500)
+        } else {
+            res.render('index', {
+                title: 'AcmeInc',
+                description: 'We sell the finest goods and services.',
+                products,
+            });
+        }
     }
-    else {
-      res.render('index', {
-        title: 'AcmeInc',
-        description: 'We sell the finest goods and services.',
-        products,
-      });
-    }
-  }
-  dbClient.getproducts({}, callBack)
+    dbClient.getproducts({}, callBack)
 });
 
 /* GET single-product information page. */
 
-router.get('/products/:urlPath', function (req, res, next) {
-  const urlPath = req.params.urlPath;
-  const callBack = (error, product) => {
-    if (error) {
-      res.sendStatus(500)
+router.get('/products/:urlPath', function(req, res, next) {
+    const urlPath = req.params.urlPath;
+    const callBack = (error, product) => {
+        if (error) {
+            res.sendStatus(500)
+        } else {
+            res.render('single-product', {
+                title: product[0].title,
+                description: `This is more about ${product[0].title}`,
+                product,
+            });
+        }
     }
-    else {
-      res.render('single-product', {
-        title: product[0].title,
-        description: `This is more about ${product[0].title}`,
-        product,
-      });
-    }
-  }
-  dbClient.getproducts({urlPath}, callBack);
+    dbClient.getproducts({ urlPath }, callBack);
 });
 
 module.exports = router;
-
-
