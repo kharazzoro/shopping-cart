c2cdd3146586d8850a972f02d6f6ff39bd0ddb17
kharazzoro <kha.1991.14.2017@gmail.com>
2017-09-13 19:27:04 +0100

    update

--- models/Products.js
+++ models/Products.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
-
 const { Schema } = mongoose;
-
 const schema = new Schema({
- 
+
     urlPath: String,
     title: String,
     price: Number,
@@ -14,28 +11,8 @@ const schema = new Schema({
         default: 1
     },
     isTopRated: Boolean
-  
- 
 });
 
 const Products = mongoose.model('products', schema);
 
 module.exports = Products;
-=======
-const { Schema } = mongoose;
-
-const schema = new Schema({
-    urlPath: String,
-    title: String,
-    price: Number,
-    rating: String,
-    commentCount: Number,
-    isTopRated: Boolean
-});
-
-
-
-const Products = mongoose.model('products', schema);
-
-module.exports = Products;
->>>>>>> e01b115e87dfde2e95346e0f54b1a71800b433fb
